a42ba5ec389fdd8e046411d94b671033e0c48911
renathamp <renatha.amelia23@gmail.com>
2024-08-21 21:56:17 +0700

    Update - ubah menu dan viewmodel

--- app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/primary/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/primary/profile/ProfileViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.ViewModel
 class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
