b5a2cc2fb5bd7e689f1f2f6c7210557e6ca44681
renathamp <renatha.amelia23@gmail.com>
2024-08-23 00:15:04 +0700

    update - add recyclerview for rekomendasi

--- app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/adapter/HorizontalRecyclerView.kt
+++ app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/adapter/HorizontalRecyclerView.kt
@@ -1,27 +1,37 @@
 package com.example.sistemcerdasrekomendasitanaman.ui.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.sistemcerdasrekomendasitanaman.R
+import com.example.sistemcerdasrekomendasitanaman.data.database.Favorite
+import com.example.sistemcerdasrekomendasitanaman.databinding.ItemFavoriteBinding
 
 class HorizontalRecyclerView: RecyclerView.Adapter<HorizontalRecyclerView.MyViewHolder>() {
 
+    private var dataList : List<Favorite> = ArrayList()
+
+    fun setData(data: List<Favorite>){
+        this.dataList = data
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_favorite, parent, false)
-        return MyViewHolder(view)
+        val itemFavoriteBinding = ItemFavoriteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(itemFavoriteBinding)
     }
 
     override fun getItemCount(): Int {
-        return 7
+        return 5
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-
+        holder.bind(dataList[position])
     }
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+    class MyViewHolder(private val binding: ItemFavoriteBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(favorite: Favorite) {
+            with(binding) {
+                tvPlantName.text = favorite.name
+            }
+        }
     }
 }
--- app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/primary/detail_tanaman/DetailTanamanActivity.kt
+++ app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/primary/detail_tanaman/DetailTanamanActivity.kt
@@ -2,12 +2,18 @@ package com.example.sistemcerdasrekomendasitanaman.ui.primary.detail_tanaman
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sistemcerdasrekomendasitanaman.R
 import com.example.sistemcerdasrekomendasitanaman.data.database.DetailTanaman
+import com.example.sistemcerdasrekomendasitanaman.data.database.DummyData
 import com.example.sistemcerdasrekomendasitanaman.databinding.ActivityDetailTanamanBinding
+import com.example.sistemcerdasrekomendasitanaman.ui.adapter.HorizontalRecyclerView
 
 class DetailTanamanActivity: AppCompatActivity() {
     private lateinit var binding: ActivityDetailTanamanBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: HorizontalRecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +37,13 @@ class DetailTanamanActivity: AppCompatActivity() {
         binding.txtTemp.text = dummyDetailTanaman.temperature
         binding.tvDetailDescription.text = dummyDetailTanaman.description
         binding.ivDetailTanaman.setImageResource(dummyDetailTanaman.imageSlide)
+
+        recyclerView = binding.rvFavorite
+        adapter = HorizontalRecyclerView()
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = adapter
+
+        adapter.setData(DummyData.favoriteItems)
 
         binding.btnBack.setOnClickListener {
             finish()
