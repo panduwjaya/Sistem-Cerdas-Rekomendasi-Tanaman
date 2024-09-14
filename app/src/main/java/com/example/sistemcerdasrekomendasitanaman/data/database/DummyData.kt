33d94e9477c7f86be7ce0ac6b6d6f5d2ea19046b
renathamp <renatha.amelia23@gmail.com>
2024-09-15 03:18:17 +0700

    add - image plant and dummy data

--- app/src/main/java/com/example/sistemcerdasrekomendasitanaman/data/database/DummyData.kt
+++ app/src/main/java/com/example/sistemcerdasrekomendasitanaman/data/database/DummyData.kt
@@ -1,5 +1,7 @@
 package com.example.sistemcerdasrekomendasitanaman.data.database
 
+import com.example.sistemcerdasrekomendasitanaman.R
+
 object DummyData {
     val favoriteItems = listOf(Favorite(1, "Aristolochia Acuminata"),
         Favorite(1, "Aristolochia Acuminata"),
@@ -12,5 +14,88 @@ object DummyData {
         Favorite(1, "Aristolochia Acuminata"),
         Favorite(1, "Aristolochia Acuminata"),
         Favorite(1, "Aristolochia Acuminata")
+    )
+
+    val detailTanaman = listOf(
+        DetailTanaman(
+            1,
+            R.drawable.jengkol_1,
+            "Archidendron pauciflorum",
+            "Jengkol, Pithecellobium jiringa",
+            "25-30℃",
+            "Jengkol adalah pohon dari keluarga Fabaceae yang dapat tumbuh hingga 20 meter. Pohon ini memiliki kulit batang halus berwarna abu-abu dan daun bipinnate. Biji jengkol sering digunakan dalam masakan Indonesia dan Malaysia, meskipun memiliki bau yang kuat. Tanaman ini tumbuh subur di hutan hujan primer dan sekunder di Asia Tenggara"
+        ),
+        DetailTanaman(
+            2,
+            R.drawable.aristolochia_acuminata_1,
+            "Aristolochia acuminata",
+            "Aristolochia indica var. magna, Aristolochia tagala",
+            "20-38℃",
+            "Aristolochia acuminata, dari keluarga Aristolochiaceae, adalah tanaman merambat hijau abadi yang berasal dari Asia Selatan dan Asia Tenggara. Tanaman ini memiliki bunga berbentuk pipa berwarna ungu-hijau dan buah berbentuk kapsul elipsoid. Aristolochia acuminata tumbuh subur di hutan hujan tropis dan monsun."
+        ),
+        DetailTanaman(
+            3,
+            R.drawable.cassia_fistula_1,
+            "Cassia fistula",
+            "Golden Shower Tree, Purging Cassia, Rajah Kayu",
+            "18–29℃",
+            "Cassia fistula, dari keluarga Fabaceae, berasal dari subkontinen India dan Asia Tenggara. Pohon ini memiliki bunga kuning cerah yang menggantung dalam kelompok panjang, menarik kupu-kupu dan burung. Selain itu, hampir semua bagian tanaman ini memiliki khasiat obat, termasuk untuk mengobati sembelit dan masalah hati."
+        ),
+        DetailTanaman(
+            4,
+            R.drawable.clerodendrum_1,
+            "Clerodendrum paniculatum",
+            "Bunga Pagoda, Clerodendrum japonicum",
+            "21-30℃",
+            "Clerodendrum paniculatum, dari keluarga Lamiaceae, berasal dari Assam hingga Vietnam. Tanaman ini memiliki bunga merah atau oranye yang tersusun dalam bentuk piramida, menarik kupu-kupu untuk penyerbukan. Daunnya besar, mengkilap, dan berurat dalam, sering kali memiliki 3-5 lobus. Tanaman ini digunakan dalam pengobatan tradisional untuk mengatasi rematik, luka, dan peradangan."
+        ),
+        DetailTanaman(
+            5,
+            R.drawable.mussaenda_frondosa_1,
+            "Mussaenda frondosa",
+            "Gardenia frondosa, Mussaenda belilla",
+            "20-38℃",
+            "Mussaenda glabra, dari keluarga Rubiaceae, berasal dari Semenanjung Malaysia dan Singapura. Tanaman ini memiliki daun hijau terang dan bunga oranye-merah yang menarik serangga untuk penyerbukan. Daunnya dapat digunakan untuk membuat teh yang bermanfaat untuk batuk."
+        ),
+        DetailTanaman(
+            6,
+            R.drawable.micromelum_minutum_1,
+            "Micromelum minutum",
+            "Aulacia falcata Lour, Bergera villosa Wall",
+            "20-38℃",
+            "Micromelum minutum, dari keluarga Rutaceae, adalah pohon kecil tropis yang dikenal dengan daun hijau mengkilap dan bunga putih harum. Tanaman ini menghasilkan buah kecil berwarna oranye-kuning dan tumbuh subur di bawah naungan hutan lebat. Micromelum minutum tersebar dari India dan Indochina hingga Australia."
+        ),
+        DetailTanaman(
+            7,
+            R.drawable.melicope_lunu_ankenda_1,
+            "Melicope lunu-ankenda",
+            "Euodia lunu-ankenda, Chabang Tiga",
+            "13-27,5℃",
+            "Melicope lunu-ankenda adalah tanaman hutan dari keluarga Rutaceae yang tumbuh di Asia, termasuk Indonesia. Tanaman ini memiliki daun trifoliat elips, bunga putih kecil, dan buah folikel kering yang berisi 1-2 biji. Selain dimanfaatkan dalam pengobatan tradisional, tanaman ini juga berpotensi digunakan sebagai bahan salad dan bumbu."
+        ),
+        DetailTanaman(
+            8,
+            R.drawable.cinnamomum_verum_1,
+            "Cinnamomum verum ",
+            "Kayu Manis, Cinnamomum zeylanicum",
+            "19–23,3℃",
+            "Cinnamomum verum, dari keluarga Lauraceae, berasal dari Sri Lanka. Pohon ini dapat tumbuh hingga 17 meter dan memiliki daun hijau gelap dengan tiga urat kuning yang khas. Bunganya kecil, berwarna putih atau krem, dan buahnya berupa drupa ungu yang mengandung satu biji. Kulit kayunya digunakan sebagai rempah kayu manis, sementara minyak dari daunnya digunakan sebagai perasa."
+        ),
+        DetailTanaman(
+            9,
+            R.drawable.uvaria_1,
+            "Uvaria grandiflora",
+            "Akar Layak, Uvaria rhodantha Hance ex Walp.",
+            "24-26℃",
+            "Uvaria grandiflora, dari keluarga Annonaceae, adalah tanaman merambat atau semak yang berasal dari Myanmar, Vietnam, dan wilayah lainnya di Asia Tenggara. Tanaman ini memiliki bunga merah gelap yang harum dan buah oranye berbentuk silinder. Daunnya hijau mengkilap dengan bentuk oblong-lanceolate. Uvaria grandiflora sering ditemukan di hutan hujan primer dan sekunder."
+        ),
+        DetailTanaman(
+            10,
+            R.drawable.saga_1,
+            "Adenanthera pavonina",
+            "Saga, Inga pavonina",
+            "25-30℃",
+            "Adenanthera pavonina, dari keluarga Fabaceae, berasal dari India, Sri Lanka, dan Asia Tenggara. Pohon ini memiliki bunga putih-kuning yang harum dan biji merah mengkilap yang sering digunakan untuk membuat perhiasan. Kayunya keras dan digunakan untuk konstruksi, pembuatan furnitur, dan kayu bakar."
+        )
     )
 }
