6068edd59e57f7b64b677815107f0d9880f27065
panduwjaya <wijayapandu388@gmail.com>
2024-10-02 13:08:22 +0700

    update - fix login and register

--- app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/auth/login/LoginActivity.kt
@@ -3,84 +3,90 @@ package com.example.sistemcerdasrekomendasitanaman.ui.auth.login
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.sistemcerdasrekomendasitanaman.R
 import com.example.sistemcerdasrekomendasitanaman.databinding.ActivityLoginBinding
+import com.example.sistemcerdasrekomendasitanaman.ui.auth.forgotpassword.ResetPasswordActivity
 import com.example.sistemcerdasrekomendasitanaman.ui.auth.register.RegisterActivity
 import com.example.sistemcerdasrekomendasitanaman.ui.onboarding.onboardingFirst.OnFirstActivity
-import com.example.sistemcerdasrekomendasitanaman.utils.show
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private val context = this@LoginActivity
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        navigateToRegister()
-        onClickLogin()
-    }
-
-    private fun onClickLogin() {
-        binding.apply {
-            loginButton.setOnClickListener {
-                startLoadingState()
-
-                val email = loginEdEmail.text.toString()
-                val password = loginEdPassword.text.toString()
-
-                val emailError = if (email.isEmpty()) getString(R.string.email_empty)
-                else if (!Patterns.EMAIL_ADDRESS.matcher(email)
-                        .matches()
-                ) getString(R.string.invalid_email)
-                else null
-
-                val passwordError = if (password.isEmpty()) getString(R.string.password_empty)
-                else if (password.length < 8) getString(R.string.invalid_password)
-                else null
-
-                emailBox.error = emailError
-                passwordBox.error = passwordError
-
-            }
-            finishLoadingState()
-            startActivity(Intent(context, OnFirstActivity::class.java))
+        binding.forgotPassword.setOnClickListener {
+            val intent = Intent(this,ResetPasswordActivity::class.java)
+            startActivity(intent)
         }
-    }
 
-    private fun navigateToRegister() {
         binding.registerHere.setOnClickListener {
-            startActivity(Intent(context, RegisterActivity::class.java))
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        loginAuth()
+    }
+
+    private fun loginAuth() {
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginEdEmail.text.toString()
+            val password = binding.loginEdPassword.text.toString()
+
+            if (email.isEmpty()) {
+                binding.loginEdEmail.error = "Email Harus Diisi"
+                binding.loginEdEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.loginEdEmail.error = "Email Tidak Valid"
+                binding.loginEdEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                binding.loginEdPassword.error = "Password Harus Diisi"
+                binding.loginEdPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password.length < 6){
+                binding.loginEdPassword.error = "Password Minimal 6 Karakter"
+                binding.loginEdPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            loginAccount(email, password)
         }
     }
 
-    private fun startLoadingState() {
-        binding.apply {
-            progressBarLogin.show(true)
-            loginEdEmail.isEnabled = false
-            loginEdPassword.isEnabled = false
-            loginButton.isEnabled = false
-        }
-    }
-
-    private fun finishLoadingState() {
-        binding.apply {
-            progressBarLogin.show(false)
-            loginEdEmail.isEnabled = true
-            loginEdPassword.isEnabled = true
-            loginButton.isEnabled = true
-        }
+    private fun loginAccount(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this,"Login Berhasil",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, OnFirstActivity::class.java)
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this,"${it.exception?.message}",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/auth/register/RegisterActivity.kt
@@ -2,17 +2,21 @@ package com.example.sistemcerdasrekomendasitanaman.ui.auth.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.sistemcerdasrekomendasitanaman.R
 import com.example.sistemcerdasrekomendasitanaman.databinding.ActivityRegisterBinding
+import com.example.sistemcerdasrekomendasitanaman.ui.auth.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-    private val context = this@RegisterActivity
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +28,53 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        navigateToLogin()
+        registAuth()
     }
-    private fun navigateToLogin(){
-        binding.loginButton.setOnClickListener {
-            startActivity(Intent(context, RegisterActivity::class.java))
+
+    private fun registAuth() {
+        auth = FirebaseAuth.getInstance()
+        binding.registerButton.setOnClickListener {
+            val email = binding.registerEdEmail.text.toString()
+            val password = binding.registerEdPassword.text.toString()
+
+            if (email.isEmpty()) {
+                binding.registerEdEmail.error = "Email Harus Diisi"
+                binding.registerEdEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.registerEdEmail.error = "Email Tidak Valid"
+                binding.registerEdEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                binding.registerEdPassword.error = "Password Harus Diisi"
+                binding.registerEdPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password.length < 6){
+                binding.registerEdPassword.error = "Password Minimal 6 Karakter"
+                binding.registerEdPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            registerAccount(email, password)
         }
+    }
+
+    private fun registerAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful){
+                    Toast.makeText(this,"Register Berhasil",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,LoginActivity::class.java)
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this,"${it.exception?.message}",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
