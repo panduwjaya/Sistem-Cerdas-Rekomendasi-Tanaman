c93e847515a7502fa2983fe5e69a93d73b55afc6
panduwjaya <wijayapandu388@gmail.com>
2024-10-04 15:36:12 +0700

    update - button in ProfileFragment

--- app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/primary/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/primary/profile/ProfileFragment.kt
@@ -1,19 +1,28 @@
 package com.example.sistemcerdasrekomendasitanaman.ui.primary.profile
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.sistemcerdasrekomendasitanaman.R
 import com.example.sistemcerdasrekomendasitanaman.databinding.FragmentProfileBinding
 import com.example.sistemcerdasrekomendasitanaman.ui.auth.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var imgUri: Uri
 
     private lateinit var auth: FirebaseAuth
 
@@ -31,6 +40,22 @@ class ProfileFragment : Fragment() {
 
         auth = FirebaseAuth.getInstance()
 
+        binding.tvChangeProfile.setOnClickListener {
+            goToCamera()
+        }
+
+        binding.btnChangePassword.setOnClickListener {
+            view.findNavController().navigate(R.id.action_navigation_profile_to_changePasswordFragment)
+        }
+
+        binding.btnHelp.setOnClickListener {
+            view.findNavController().navigate(R.id.action_navigation_profile_to_helpFragment)
+        }
+
+        binding.btnAppinfo.setOnClickListener {
+            view.findNavController().navigate(R.id.action_navigation_profile_to_appInfoFragment)
+        }
+
         binding.btnLogout.setOnClickListener {
             auth.signOut()
             val intent = Intent(context,LoginActivity::class.java)
@@ -39,9 +64,50 @@ class ProfileFragment : Fragment() {
         }
     }
 
+    private fun goToCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {intent ->
+            activity?.packageManager?.let {
+                intent?.resolveActivity(it).also {
+                    startActivityForResult(intent,REQ_CAM)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQ_CAM && resultCode == RESULT_OK){
+            val imgBitmap = data?.extras?.get("data") as Bitmap
+            uploadImgToFirebase(imgBitmap)
+        }
+    }
+
+    private fun uploadImgToFirebase(imgBitMap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        val ref = FirebaseStorage.getInstance().reference.child("img_user/${FirebaseAuth.getInstance().currentUser?.email}")
+        imgBitMap.compress(Bitmap.CompressFormat.JPEG,100,baos)
+
+        val img = baos.toByteArray()
+        ref.putBytes(img)
+            .addOnCompleteListener{
+                if (it.isSuccessful){
+                    ref.downloadUrl.addOnCompleteListener { Task ->
+                        Task.result.let { Uri ->
+                            imgUri = Uri
+                            binding.ivProfile.setImageBitmap(imgBitMap)
+                        }
+                    }
+                }
+            }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object{
+        const val REQ_CAM = 100
     }
 }
