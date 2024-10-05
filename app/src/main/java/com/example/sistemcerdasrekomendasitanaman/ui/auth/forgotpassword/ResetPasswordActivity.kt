52ff60d979c60b991a0f2a103c67f2e813568fa2
panduwjaya <wijayapandu388@gmail.com>
2024-10-05 21:38:10 +0700

    update - ResetPasswordActivity done

--- app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/auth/forgotpassword/ResetPasswordActivity.kt
+++ app/src/main/java/com/example/sistemcerdasrekomendasitanaman/ui/auth/forgotpassword/ResetPasswordActivity.kt
@@ -2,35 +2,59 @@ package com.example.sistemcerdasrekomendasitanaman.ui.auth.forgotpassword
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.sistemcerdasrekomendasitanaman.R
 import com.example.sistemcerdasrekomendasitanaman.databinding.ActivityResetPasswordBinding
-import com.example.sistemcerdasrekomendasitanaman.ui.auth.register.RegisterActivity
+import com.example.sistemcerdasrekomendasitanaman.ui.auth.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class ResetPasswordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityResetPasswordBinding
-    private val context = this@ResetPasswordActivity
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityResetPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        resetPassword()
-    }
-
-    private fun resetPassword(){
         binding.resetButton.setOnClickListener {
-            startActivity(Intent(context, RegisterActivity::class.java))
+            val email = binding.resetEdEmail.text.toString()
+            val edtEmail = binding.resetEdEmail
+
+            if(email.isEmpty()){
+                edtEmail.error = "Email Tidak Boleh Kosong"
+                edtEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                edtEmail.error = "Email Tidak Valid"
+                edtEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            auth.sendPasswordResetEmail(email).addOnCompleteListener{
+                if (it.isSuccessful){
+                    Toast.makeText(this,"Email Reset Password Telah Dikirim",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else{
+                    Toast.makeText(this,"${it.exception?.message}",Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
